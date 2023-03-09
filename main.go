f15e48667a1b5f7bcdfae0295af49b85980b7048
EvanTheBoy <evantheboy945@gmail.com>
2023-03-09 12:15:32 +0800

    完成部分配置文件的编写，现在开始连接数据库

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"fmt"
+	"gin_vue_blog/gin_vue_blog_server/core"
+	"gin_vue_blog/gin_vue_blog_server/global"
+)
+
+func main() {
+	core.InitCore()
+	fmt.Println(global.Config)
+}
