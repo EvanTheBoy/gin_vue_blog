b5e5451b0342786ff9de8bb4d9f513fc693f2daa
EvanTheBoy <evantheboy945@gmail.com>
2023-03-12 18:58:37 +0800

    编写完部分logrus.go代码

--- core/logrus.go
+++ core/logrus.go
@@ -1,8 +1,57 @@
 package core
 
+import (
+	"bytes"
+	"fmt"
+	"gin_vue_blog/gin_vue_blog_server/global"
+	"github.com/sirupsen/logrus"
+	"path"
+)
+
 const (
 	red    = 31
 	yellow = 33
 	blue   = 36
 	gray   = 37
 )
+
+type LogFormatter struct {
+}
+
+func (t *LogFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	//根据不同的level展示颜色
+	var levelColor int
+	switch entry.Level {
+	case logrus.DebugLevel, logrus.TraceLevel:
+		levelColor = gray
+	case logrus.WarnLevel:
+		levelColor = yellow
+	case logrus.ErrorLevel, logrus.FatalLevel, logrus.PanicLevel:
+		levelColor = red
+	default:
+		levelColor = blue
+	}
+	var b *bytes.Buffer
+	if entry.Buffer != nil {
+		b = entry.Buffer
+	} else {
+		b = &bytes.Buffer{}
+	}
+	log := global.Config.Logger
+
+	//自定义日期格式
+	timestamp := entry.Time.Format("2006-01-02 15:04:05")
+	if entry.HasCaller() {
+		//自定义文件路径
+		funcVal := entry.Caller.Function
+		fileVal := fmt.Sprintf("%s:%d", path.Base(entry.Caller.File), entry.Caller.Line)
+		fmt.Fprintf(b, "%s[%s] \x1b[%dm[%s]\x1b[0m %s %s %s\n",
+			log.Prefix, timestamp, levelColor, entry.Level, fileVal, funcVal, entry.Message)
+	} else {
+		fmt.Fprintf(b, "%s[%s] \x1b[%dm[%s]\x1b[0m %s\n",
+			log.Prefix, timestamp, levelColor, entry.Level, entry.Message)
+	}
+	return b.Bytes(), nil
+}
+
+
