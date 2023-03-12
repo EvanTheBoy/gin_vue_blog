5871c4dbc2a8b55b39879c467b38dd1860930440
EvanTheBoy <evantheboy945@gmail.com>
2023-03-12 18:40:54 +0800

    删除了冗余的打印测试代码，开始编写logrus.go的代码

---
+++ core/logrus.go
@@ -0,0 +1,8 @@
+package core
+
+const (
+	red    = 31
+	yellow = 33
+	blue   = 36
+	gray   = 37
+)
--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"gin_vue_blog/gin_vue_blog_server/core"
 	"gin_vue_blog/gin_vue_blog_server/global"
 )
 
 func main() {
+	//读取配置文件
 	core.InitCore()
+	//连接数据库
 	global.DB = core.InitGorm()
-	fmt.Println(global.DB)
+
 }
