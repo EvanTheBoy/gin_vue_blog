c65f30051ea402778e0c2b30333925f07772a5b0
EvanTheBoy <evantheboy945@gmail.com>
2023-03-12 19:13:53 +0800

    完成logrus.go代码的编写

--- core/logrus.go
+++ core/logrus.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"gin_vue_blog/gin_vue_blog_server/global"
 	"github.com/sirupsen/logrus"
+	"os"
 	"path"
 )
 
@@ -45,13 +46,42 @@ func (t *LogFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 		//自定义文件路径
 		funcVal := entry.Caller.Function
 		fileVal := fmt.Sprintf("%s:%d", path.Base(entry.Caller.File), entry.Caller.Line)
-		fmt.Fprintf(b, "%s[%s] \x1b[%dm[%s]\x1b[0m %s %s %s\n",
+		_, err := fmt.Fprintf(b, "%s[%s] \x1b[%dm[%s]\x1b[0m %s %s %s\n",
 			log.Prefix, timestamp, levelColor, entry.Level, fileVal, funcVal, entry.Message)
+		if err != nil {
+			return nil, err
+		}
 	} else {
-		fmt.Fprintf(b, "%s[%s] \x1b[%dm[%s]\x1b[0m %s\n",
+		_, err := fmt.Fprintf(b, "%s[%s] \x1b[%dm[%s]\x1b[0m %s\n",
 			log.Prefix, timestamp, levelColor, entry.Level, entry.Message)
+		if err != nil {
+			return nil, err
+		}
 	}
 	return b.Bytes(), nil
 }
 
+func InitDefaultLogger() {
+	//全局logrus
+	logrus.SetOutput(os.Stdout)
+	logrus.SetReportCaller(global.Config.Logger.ShowLine)
+	logrus.SetFormatter(&LogFormatter{})
+	level, err := logrus.ParseLevel(global.Config.Logger.Level)
+	if err != nil {
+		level = logrus.InfoLevel
+	}
+	logrus.SetLevel(level) //设置最低的level
+}
 
+func InitLogger() *logrus.Logger {
+	mLog := logrus.New()                                        //新建一个实例
+	mLog.SetOutput(os.Stdout)                                   //设置输出类型
+	mLog.SetFormatter(&LogFormatter{})                          //开启返回函数名和行号
+	level, err := logrus.ParseLevel(global.Config.Logger.Level) //设置自己定义的Formatter
+	if err != nil {
+		level = logrus.InfoLevel
+	}
+	mLog.SetLevel(level)
+	InitDefaultLogger()
+	return mLog
+}
